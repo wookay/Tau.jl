06fb7a3e581ba200d43a22b0f96d7513db3bb09f
WooKyoung Noh <wookay.noh@gmail.com>
2017-04-21 21:53:42 +0900

    init

--- src/Tau.jl
+++ src/Tau.jl
@@ -1,5 +1,28 @@
+__precompile__()
+
 module Tau
 
-# package code goes here
+export tau, τ
+
+const τ = Irrational{:τ}()
+Base.convert(::Type{BigFloat}, ::Irrational{:τ}) = BigFloat(2pi)
+Base.convert(::Type{Float64}, ::Irrational{:τ}) = 2pi
+Base.convert(::Type{Float32}, ::Irrational{:τ}) = Float32(2pi)
+
+"""
+    tau
+    τ
+
+The constant τ.
+
+```jldoctest
+julia> tau
+τ = 6.283185307179586...
+```
+"""
+const tau = τ
+
+Base.:(==)(::Irrational{:τ}, y::Float64) = 2pi == y
+Base.:(==)(x::Float64, ::Irrational{:τ}) = x == 2pi
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using Tau
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@test 2pi == tau
+@test 2π == τ
+@test isa(tau, Irrational{:τ})
+@test 1.0 == cos(τ)
+@test 2acos(-log(e)) == τ
+@test real(4acos(log(im))) == τ
+@test imag(4log(im)) == τ
+@test 360 == rad2deg(τ)
+@test 360/τ == rad2deg(1)
+@test τ/360 == deg2rad(1)
